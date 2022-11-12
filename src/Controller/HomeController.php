cea87ffd8bbd07594ea9121ca481cc144b622bf7
slipaclous <gauthier.minor@gmail.com>
2022-11-12 15:25:37 +0100

    fixtures marques + venteController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\MarquesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(MarquesRepository $repo): Response
     {
+        $marque = $repo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'marques' => $marque,
         ]);
     }
 }
